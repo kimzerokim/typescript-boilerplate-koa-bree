9da9bf8a382ed181a371faf39ebfd7c4181007a4
Iksung Jo <iksung.jo@chainanatomy.com>
2023-05-10 16:18:59 +0900

    Fix exit process correctly when server is stopped by error.

--- src/index.ts
+++ src/index.ts
@@ -17,53 +17,58 @@ function typescript_worker() {
 }
 
 (async () => {
-  logger.info(`${path.resolve(__dirname)}`);
-  logger.info(`PM2 Instance Num: ${config.PM2_INSTANCE_NUM}`);
-  logger.info(`PM2 Instance Index: ${config.PM2_INDEX}`);
-  logger.info('#####################');
-  if (config.PM2_INDEX === 0) {
-    logger.info('Using following configuration...');
-    Object.keys(config).forEach((key: string) => {
-      const valueToDisplay = key === 'DB_PASSWORD' ? '*' : (config[key] as string);
-      logger.info(`${key}: ${valueToDisplay}`);
-    });
-  }
+  try {
+    logger.info(`${path.resolve(__dirname)}`);
+    logger.info(`PM2 Instance Num: ${config.PM2_INSTANCE_NUM}`);
+    logger.info(`PM2 Instance Index: ${config.PM2_INDEX}`);
+    logger.info('#####################');
+    if (config.PM2_INDEX === 0) {
+      logger.info('Using following configuration...');
+      Object.keys(config).forEach((key: string) => {
+        const valueToDisplay = key === 'DB_PASSWORD' ? '*' : (config[key] as string);
+        logger.info(`${key}: ${valueToDisplay}`);
+      });
+    }
 
-  // Establish DB connection
-  await dataSource.initialize();
-  logger.info('DB connection established');
+    // Establish DB connection
+    await dataSource.initialize();
+    logger.info('DB connection established');
 
-  // Worker scheduler
-  const bree = new Bree({
-    logger: false,
-    root: false,
-    jobs: config.isDevMode
-      ? [
-          {
-            name: 'testJob-ts',
-            path: typescript_worker,
-            interval: '1m',
-            worker: {
-              workerData: {
-                __filename: path.join('src/job/', 'testJob.ts'),
+    // Worker scheduler
+    const bree = new Bree({
+      logger: false,
+      root: false,
+      jobs: config.isDevMode
+        ? [
+            {
+              name: 'testJob-ts',
+              path: typescript_worker,
+              interval: '1m',
+              worker: {
+                workerData: {
+                  __filename: path.join('src/job/', 'testJob.ts'),
+                },
               },
             },
-          },
-        ]
-      : [
-          {
-            name: 'testJob-js',
-            interval: '1m',
-            path: path.join(__dirname, 'job/', 'testJob.js'),
-          },
-        ],
-  });
-  await bree.start();
+          ]
+        : [
+            {
+              name: 'testJob-js',
+              interval: '1m',
+              path: path.join(__dirname, 'job/', 'testJob.js'),
+            },
+          ],
+    });
+    await bree.start();
 
-  // Koa webserver
-  const app: Koa = await createApp();
-  const port: number = parseInt(process.env.PORT || '4000', 10);
-  const server = http.createServer(app.callback());
-  server.listen(port);
-  logger.info(`Server has started on port: ${port}`);
+    // Koa webserver
+    const app: Koa = await createApp();
+    const port: number = parseInt(process.env.PORT || '4000', 10);
+    const server = http.createServer(app.callback());
+    server.listen(port);
+    logger.info(`Server has started on port: ${port}`);
+  } catch (err) {
+    logger.error('Server has exited: %o', (err as Error).stack || err);
+    process.exit();
+  }
 })();
